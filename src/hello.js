d6c49fc96b138764870431b4500e5554d6865a6e
yourLogic01 <2010631170004@student.unsika.ac.id>
2024-12-03 10:55:40 +0700

    feat : belajar basic routing express js

--- src/hello.js
+++ src/hello.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/maulana", (req, res) => {
+  res.send("Hello Maulana!");
+});
+
 app.listen(3000, () => {
   console.info("Server running on port 3000");
 });
