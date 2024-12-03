ca313d63e9fae99511649efeb20c6cff2bdc6b16
yourLogic01 <2010631170004@student.unsika.ac.id>
2024-12-03 11:27:39 +0700

    feat : request url

---
+++ test/request-url.test.js
@@ -0,0 +1,25 @@
+import express from "express";
+import request from "supertest";
+
+const app = express();
+
+app.get("/hello/world", (req, res) => {
+  res.json({
+    path: req.path,
+    originalUrl: req.originalUrl,
+    hostname: req.hostname,
+    protocol: req.protocol,
+    secure: req.secure,
+  });
+});
+
+test("Test Request URL", async () => {
+  const response = await request(app).get("/hello/world").query({ name: "maulana" });
+  expect(response.body).toEqual({
+    path: "/hello/world",
+    originalUrl: "/hello/world?name=maulana",
+    hostname: "127.0.0.1",
+    protocol: "http",
+    secure: false,
+  });
+});
